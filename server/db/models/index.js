205da216d5b1fe09d4160c216650f78a29e39ac0
sooin.c <sooinc90@gmail.com>
2020-03-27 11:18:56 -0400

    added route to get dish with ingredients

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,23 +2,13 @@ const User = require('./user')
 const Dish = require('./dish')
 const Ingredient = require('./ingredient')
 const UserDish = require('./userDish')
-const DishIngredient = require('./DishIngredient')
+const DishIngredient = require('./dishIngredient')
 
-UserDish.belongsTo(User, {foreignKey: 'userId', through: UserDish})
-UserDish.belongsTo(Dish, {foreignKey: 'dishId', through: UserDish})
+Dish.belongsToMany(Ingredient, {through: DishIngredient})
+Ingredient.belongsToMany(Dish, {through: DishIngredient})
 
-// DishIngredient.belongsTo(Dish, {foreignKey: 'dishId', through: DishIngredient})
-// DishIngredient.belongsTo(Ingredient, { foreignKey: 'ingredientId', as: 'DishIngre1', through: DishIngredient})
-
-// User.belongsToMany(Dish, { through: UserDish}) //'user-dish'
-// Dish.belongsToMany(User, { through: UserDish}) //'user-dish'
-// Dish.hasMany(UserDish)
-
-Dish.belongsToMany(Ingredient, {as: 'DishIngre1', through: DishIngredient})
-Ingredient.belongsToMany(Dish, {as: 'DishIngre2', through: DishIngredient})
-
-// User.belongsToMany(Ingredient, {through: 'user-ingredient'})
-// Ingredient.belongsToMany(User, {through: 'user-ingredient'})
+UserDish.belongsTo(User, {foreignKey: 'userId'}) //, through: UserDish
+UserDish.belongsTo(Dish, {foreignKey: 'dishId'}) //, through: UserDish
 
 module.exports = {
   User,
